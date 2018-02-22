9a0dee2b6c37ea766dcc9fe79b1ddab0e699c9ba
Alan Chusuei <alan.chusuei@gmail.com>
2018-02-22 15:45:08 -0500

    following the flask tutorial (#1)
    * added new basic files
    * add application context level db connections
    * add db connectors
    * ignore db files created my sqlite

---
+++ seonmul/__init__.py
@@ -0,0 +1,1 @@
+from .seonmul import app
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='seonmul',
+    packages=['seonmul'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
