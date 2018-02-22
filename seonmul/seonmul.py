2ba60ac9b482cf2942fa678d69ca610b35460eae
Alan Chusuei <alan@compstak.com>
2018-02-21 21:48:57 -0500

    added new basic files

---
+++ seonmul/seonmul.py
@@ -0,0 +1,22 @@
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+
+app = Flask(__name__) # create the application instance :)
+app.config.from_object(__name__) # load config from this file , flaskr.py
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'seonmul.db'),
+    SECRET_KEY='3c627aa058085da3791e6a1f0e8848b1eff4abfa',
+    USERNAME='admin',
+    PASSWORD='3239ec4f4d8121bb524fe59b66992b31b5411a52'
+))
+app.config.from_envvar('SEONMUL_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
